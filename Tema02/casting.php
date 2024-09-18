3ad65222c2f7d6db76af92ce54efaed90db96bf4
HectorFernandezRico <hectorfernandezrico@gmail.com>
2024-09-18 17:18:41 +0200

    Primer dia PHP.

---
+++ Tema02/casting.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = 3.2;
+
+    echo $a/2;
+
+    echo "<br>";
+
+    $n1 = "34hhhh";
+
+    echo $n1 + 3;
+    echo "<br>";
+    
+?>
---
+++ Tema02/operadores.php
@@ -0,0 +1,11 @@
+<?php 
+    $a = "ejejiif";
+    $b = 20;
+    if ($a != $b) echo "son distintos";
+    else echo "no son distintos";
+    echo "<br>";
+
+    if ($a && $b) echo "es true";
+    else echo "es false";
+    echo "<br>";
+?>
