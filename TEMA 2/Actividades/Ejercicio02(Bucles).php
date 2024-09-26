90ecc67084ef4088a904dced1185c7078eae2276
HectorFernandezRico <hectorfernandezrico@gmail.com>
2024-09-26 20:25:02 +0200

    Ejercicio02 completo.

--- TEMA 2/Actividades/Ejercicio02(Bucles).php
+++ TEMA 2/Actividades/Ejercicio02(Bucles).php
@@ -15,18 +15,4 @@
     }
     while ($edad < $juventud) ;
            
-
-    /*
-    do {
-        if ($edad != 40) {
-            for ($año = 0; $año <= 2010; $año++) {
-                $edad++;
-                echo "En el año $año tengo $edad años y soy joven <br>";
-            }
-        } else if ($edad == 40) {
-            echo "En el año $año tengo $edad años y se acabó la juventud";
-        }
-    } while ( $edad != 40); 
-    */
-
 ?>
