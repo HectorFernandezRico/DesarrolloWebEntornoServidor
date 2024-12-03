3d8f39a90f931178114728eaebdc32f04d83afee
HectorFernandezRico <hectorfernandezrico@gmail.com>
2024-12-03 20:50:20 +0100

    Terminar Json.

--- TEMA 5/Actividades/Tema5-Json/Ejercicio01_Json.php
+++ TEMA 5/Actividades/Tema5-Json/Ejercicio01_Json.php
@@ -2,11 +2,42 @@
     session_start();
     require_once("pdo.php");
 
-    $resultado = $pdo -> query("SELECT * FROM usuarios");
+    function mostrarTabla($pdo){
+    $crearFichero = "./files/socios.json";
+    $tabla = "";
+    $array = [];
+    try {
+        $query = "SELECT * FROM usuarios";
+        $resultado = $pdo->query($query);
+        while ($fila = $resultado->fetch(PDO::FETCH_ASSOC)) {
+            // Concatenamos las filas a la tabla en vez de sobrescribirla
+            $tabla .= '<tr>';
+            $tabla .= '<td>' . $fila["usuario_id"] . "</td>";
+            $tabla .= '<td>' . $fila["nombre"] . "</td>";
+            $tabla .= '<td>' . $fila["email"] . "</td>";
+            $tabla .= '<td>' . $fila["password"] . "</td>";            
+            $tabla .= '</tr>';
 
-    while ($fila = $resultado -> fetch (PDO::FETCH_ASSOC)) {     
-        $filas[] = $fila;
+            $array[] = $fila;
+            
+        }
+
+        $fichero = fopen($crearFichero, "w+");
+        $json = json_encode($array, JSON_PRETTY_PRINT);
+        fwrite($fichero, $json);
+
+        fclose($fichero);
+
+    } catch (Exception $ex) {
+        $_SESSION['mensaje'] = "No se ha podido visualizar la tabla: " . $ex->getMessage();
+        error_log("ERROR: en el programa" . $ex->getFile() .  " En la linea " .
+            $ex->getFile() . " ERRORCODE: " . $ex->getCode() . 
+            "ErrorMessage: " . $ex->getMessage());
     }
+    return $tabla;
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -23,16 +54,22 @@
     <br>
     <h3>Contenido de la tabla</h3>
     <table border="2px">    
-            <?php
-            foreach ($filas as $indice => $fila) {
-                echo"<tr>";
-                foreach ($fila as $indice2 => $valor) {
-                    echo "<td>".$valor."</td>"     ;           }
-               
-                echo"</tr>";
-
-            }
-            ?>
+            <?=mostrarTabla($pdo)?>
     </table>
+    <br>
+    <h3>JSON resultante</h3>
+    <br>
+    <div>
+        <form method="post">
+            <p>
+                <label for="">Fichero a guardar: </label>
+                <input type="text" name="json">
+                <input type="submit" name="convertir" value="Convertir">
+            </p>
+        </form>
+    </div>
+    <br>
+    <h3>JSON Resultante</h3>
+    <br>
 </body>
 </html>
